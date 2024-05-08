634d8e32a31d2e1cbd01a11dc5d3d4157538669a
Sanu334 <maadarabuu132@gmail.com>
2024-05-08 15:14:13 +0530

    login route implemented with jwt as reesponse

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const users = require("./routes/users");
-
+const auth = require("./routes/auth");
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/register", users);
+app.use("/api/login", auth);
 
 app.get("/test/", (req, res) => {
   res.send("api working");
