b6c7dab6c40198614f803e3bbd8aa4b3d2954967
Sanu334 <maadarabuu132@gmail.com>
2024-05-08 14:43:55 +0530

    users reistration route complete

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const users = require("./routes/users");
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/register", users);
 
 app.get("/test/", (req, res) => {
   res.send("api working");
--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = require("./app");
 port = process.env.PORT || 5000;
 mongoose
   .connect(
-    "mongodb+srv://test:1322001Maajin@cluster0.ione1sb.mongodb.net/Vidly?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://test:1322001Maajin@cluster0.ione1sb.mongodb.net/ToDo?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     console.log("connected to cloud database");
