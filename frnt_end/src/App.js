487ab251f7046f1db78180346eeb956fe376b021
Sanu334 <maadarabuu132@gmail.com>
2024-05-08 11:43:27 +0530

    added file structure and unprotected routes

--- frnt_end/src/App.js
+++ frnt_end/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
-
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Main from "./components/mainpage";
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/main" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ frnt_end/src/components/login.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  return <></>;
+}
+
+export default Login;
