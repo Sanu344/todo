f655f79288efc72d5bf6ba4f88b55f914c6f0a23
Sanu334 <maadarabuu132@gmail.com>
2024-05-08 13:48:12 +0530

    all pages added

--- frnt_end/src/components/mainpage.js
+++ frnt_end/src/components/mainpage.js
@@ -1,7 +1,79 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import "./mainpage.css";
+
 function Main() {
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+  const onSubmit = (task) => {
+    console.log(task.todoAddTask);
+  };
+
+  let arr = [1, 2, 2, 3];
   return (
     <>
-      <h1>Main Page</h1>
+      <div className="todoMain">
+        <div className="todoTop">
+          <h1 className="todoUser">Main Page </h1>
+        </div>
+        <div className="todoSide">
+          <center>
+            {" "}
+            <div className="todoTxt">
+              <p className="todoTxtList">To Do List</p>
+              <p className="todoTxtHistory">Hisotory</p>
+            </div>
+          </center>
+          <center>
+            <div
+              className="todoLogout"
+              onClick={() => {
+                localStorage.clear();
+                navigate("/");
+              }}
+            >
+              Logout
+            </div>
+          </center>
+        </div>
+        <div className="todoAddTask">
+          <form onSubmit={handleSubmit(onSubmit)} className="todoAddTaskForm">
+            <input
+              className="todoAddTaskInput"
+              {...register("todoAddTask")}
+              type="text"
+              placeholder="Add Task"
+            />
+            <button className="todoButton" type="submit">
+              Add Task
+            </button>
+          </form>
+        </div>
+        <div className="todoDisplayTask">
+          <div className="todoHeading">
+            <div className="tableHeading">Activity</div>
+            <div className="tableHeading">Status</div>
+            <div className="tableHeading">Time Taken</div>
+            <div className="tableHeading">Action</div>
+          </div>
+          <table className="table">
+            <tbody>
+              {arr.map((items, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="tableHeading">{items}</td>
+                    <td className="tableHeading">{items}</td>
+                    <td className="tableHeading">{items}</td>
+                    <td className="tableHeading">{items}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 }
